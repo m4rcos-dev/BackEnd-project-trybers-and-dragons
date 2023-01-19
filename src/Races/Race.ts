1fa9887a24af8123bd891b59487893b6245233ae
m4rcos-dev <dev.marcospereira@gmail.com>
2023-01-18 22:24:21 -0300

    :sparkles: feat: Requisito 01

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Race;
