a4386621e368d2625280e6010381ad09585e618e
m4rcos-dev <dev.marcospereira@gmail.com>
2023-01-21 10:11:07 -0300

    :sparkles: feat: Requisito 11

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,24 @@
+// import Character from '../Character';
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  private _monster: [Fighter | SimpleFighter];
+
+  constructor(player: Fighter, monster: [Fighter | SimpleFighter]) {
+    super(player);
+    this._monster = monster;
+  }
+
+  fight(): number {
+    this._monster.forEach((monster) => {
+      while (this.player.lifePoints > 0 && monster.lifePoints > 0) {
+        this.player.attack(monster);
+        if (monster.lifePoints > 0) monster.attack(this.player);
+      }
+    });
+    return super.fight();
+  }
+}
+
+export default PVE;
--- src/utils.ts
+++ src/utils.ts
@@ -4,4 +4,11 @@ function getRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
 }
 
+// function validAttack PVE(currentMonster, currentPlayer) {
+//     if (currentMonster.every((monster) => monster.lifePoints > 0)) {
+//       currentMonster[i]
+//         .attack(currentPlayer);
+//     }
+// }
+
 export default getRandomInt;
